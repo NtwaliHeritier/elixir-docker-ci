1ce12ee9e16e3ca0eac01b70c70db13bd1bd78cb
NtwaliHeritier <heritierntwali@gmail.com>
2021-08-09 02:50:32 +0200

    third feature commit

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Crud.MixProject do
     [
       app: :crud,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
